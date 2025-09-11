3e77b842c82e7cd5ce76d97d22ddca9e61b630d4
omkar1kadam <omkarakadam2408@gmail.com>
2025-09-11 15:13:41 +0530

    Create template.cpp

---
+++ template.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+#include<string>
+#include<iomanip>
+#include<vector>
+#include<algorithm>
+using namespace std;
+
+int main(){
+  cout << "Code is Running" << endl;
+  return 0;
+}
