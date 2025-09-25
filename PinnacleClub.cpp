5542b6f503e3e7ba973125c27d96e5d69ba20ce4
Omkar Kadam <omkarakadam2408@gmail.com>
2025-09-25 16:17:40 +0530

    Update PinnacleClub.cpp
    Not working

--- PinnacleClub.cpp
+++ PinnacleClub.cpp
@@ -7,6 +7,7 @@ using namespace std;
 struct Student{
 	int PRN;
 	string name;
+	string memberType;
 	struct Student *next;
 };
 
@@ -20,21 +21,33 @@ class PinnacleClub{
 			headSYB = NULL;
 		}
 		
-		void addNode(int prn,string name){
+		void addNode(int prn,string name,string memT){
 			Student *p;
 			p = new Student;
 			p->PRN=prn;
+			p->memberType=memT;
 			p->name=name;
+			p->next=NULL;
 			
 			if(headSYA == NULL){
 				headSYA = p;
 			} else {
-				Student *curr;
-				curr = headSYA;
-				while(curr->next != NULL){
-					curr=curr->next;
+			
+				if (p->memberType=="regular") {
+					Student *curr,*back;
+					back = curr = headSYA;
+					while(curr->next != NULL){
+						back = curr;
+						curr=curr->next;
+					}
+					if (curr->memberType=="sec"){
+						back->next=p;
+						p->next=curr;
+					} else {
+						curr->next=p;
+					}
+					
 				}
-				curr->next=p;
 			}
 		}
 		
@@ -43,7 +56,7 @@ class PinnacleClub{
 			curr = headSYA;
 			cout<<"\nHeadSYA-->";
 			while(curr){
-				cout<<"["<<curr->PRN<<"|"<<curr->name<<"]-->";
+				cout<<"["<<curr->PRN<<"|"<<curr->name<<"|"<<curr->memberType<<"]-->";
 				curr=curr->next;
 			}
 			cout<<"END"<<endl;
@@ -52,8 +65,11 @@ class PinnacleClub{
 
 int main(){
 	PinnacleClub stud;
-	stud.addNode(123,"omkar");
-	stud.addNode(456,"omkar2");
+	stud.addNode(123,"omkar","regular");
+	
+	stud.addNode(789,"omkar3","regular");
+	stud.addNode(1011,"omkar4","regular");
+	
 	stud.displayList();
 	return 0;
 }
