def5d519de99a376a7f5f8465bce9c7558f10a89
Omkar Kadam <omkarakadam2408@gmail.com>
2025-10-06 10:40:54 +0530

    Update PinnacleClub.cpp

--- PinnacleClub.cpp
+++ PinnacleClub.cpp
@@ -71,6 +71,49 @@ public:
             p->next = curr;
         }
     }
+    
+    void deleteNode(int prn) {
+        Student *curr,*prev;
+        
+        if (headSYA == NULL){
+            cout << "\nList Empty";
+        } else {
+        	curr = headSYA;
+            while(curr != NULL){
+                
+                if (curr->PRN == prn) {
+                    cout << "found: " << curr->PRN;
+                    break;
+                }
+                prev = curr;
+                curr = curr->next;
+            }
+            if (curr == NULL) {
+                cout << "PRN not found";
+            } else {
+                // if the node is first node as well as last node 
+                if (headSYA == curr && curr->next  == NULL){
+                    headSYA = NULL;
+                    delete curr;
+                }
+                // if the node is first followed by another nodes
+                else if (headSYA == curr && curr->next != NULL) {
+                    headSYA = curr->next;
+                    delete curr;
+                }
+                // if the node is the last node 
+                else if (headSYA != curr && curr->next == NULL) {
+                    prev->next = NULL;
+                    delete curr;
+                }
+                // if the node is in middle node
+                else {
+                    prev->next = curr->next;
+                    delete curr;
+                }
+            }
+        }
+    }
 
     
     void displayList() {
@@ -87,11 +130,21 @@ public:
 int main() {
     PinnacleClub stud;
 
-    stud.addNode(123, "secretary", "sec");
-    stud.addNode(789, "omkar3", "regular");
-    stud.addNode(456, "President", "pres");
-    stud.addNode(1011, "omkar4", "regular");
-
+    stud.addNode(101112, "secretary", "sec");
+    stud.addNode(456, "omkar3", "regular");
+    stud.addNode(123, "President", "pres");
+    stud.addNode(789, "omkar4", "regular");
     stud.displayList();
+    stud.deleteNode(456);
+    stud.displayList();
+
     return 0;
 }
+
+
+
+
+
+
+
+
