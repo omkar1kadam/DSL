d034be0106f0a14f0e9e57f50768be619b2f2b44
Omkar Kadam <omkarakadam2408@gmail.com>
2026-02-24 12:56:57 +0530

    Update bst_Scratch.java

--- bst_Scratch.java
+++ bst_Scratch.java
@@ -5,10 +5,24 @@ class Main {
         bt.addNode(10);
         bt.addNode(5);
         bt.addNode(15);
-        bt.display();
-        bt.search(15);
-        bt.delete(15);
-        bt.display();
+        bt.addNode(3);
+        bt.addNode(7);
+        bt.addNode(12);
+        bt.addNode(18);
+        bt.addNode(1);
+        bt.addNode(4);
+        bt.addNode(6);
+        bt.addNode(8);
+        bt.addNode(11);
+        bt.addNode(13);
+        bt.addNode(17);
+        bt.addNode(20);
+        //bt.delete(15);
+        //bt.display();
+        System.out.println("bfs: ");
+        bt.bfs();
+        System.out.println("dfs: ");
+        bt.dfs();
     }
 }
 class Node{
@@ -113,8 +127,8 @@ class BinaryTree{
             return;
         }
         System.out.print(root.data+" ");
-        inOrder(root.left);
-        inOrder(root.right);
+        preOrder(root.left);
+        preOrder(root.right);
     }
     void inOrder(Node root){
         if (root == null){
@@ -130,11 +144,98 @@ class BinaryTree{
             return;
         }
         
-        inOrder(root.left);
-        inOrder(root.right);
+        postOrder(root.left);
+        postOrder(root.right);
         System.out.print(root.data+" ");
     }
-
+    void bfs(){
+        if (root == null){
+            return;
+        }
+        Queue q = new Queue();
+        q.enqueue(root);
+        
+        while(!q.isEmpty()){
+            Node current = q.dequeue();
+            System.out.print(current.data+" ");
+            if(current.left != null){
+                q.enqueue(current.left);
+            }
+            if(current.right != null){
+                q.enqueue(current.right);
+            }
+        }
+    }
+    void dfs(){
+        
+    }
+}
+class Queue {
+    Node[] data = new Node[100];
+    int front;
+    int rear;
+    Queue(){
+        front = -1;
+        rear = -1;
+    }
+    void enqueue(Node value){
+        if (front == -1){
+            front ++;
+        }
+        if (!isFull()){
+            data[++rear] = value;
+        }else{
+            System.out.println("Queue is full");
+        }
+    }
+    Node dequeue(){
+        Node val;
+        if(!isEmpty()){
+            val = data[front];
+            if (front == rear){
+                front = rear = -1;
+            }
+            else front++;
+            return val;
+        }
+        return null;
+    }
+    boolean isEmpty(){
+		return rear == -1; 
+	}
+	boolean isFull(){
+		return rear == 100-1;
+	}
+}
+class Stack{
+    Node[] data = new Node[100];
+    int top;
+    Stack(){
+        top = -1;
+    }
+    void push(Node value){
+        if(top == -1){
+            top++;
+        }
+        if(!isFull()){
+            data[top] = value;
+        } else {
+            System.out.println("stack is full");
+        }
+    }
+    Node pop(){
+        if(isEmpty()){
+            System.out.println("stack is empty");
+            return null;
+            }
+        return data[--top];
+    }
+    boolean isFull(){
+        return top >= 100;
+    }
+    boolean isEmpty(){
+        return top == -1;
+    }
 }
 
 
